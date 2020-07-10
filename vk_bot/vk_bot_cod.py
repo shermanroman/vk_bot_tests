a7a0cb3a6cb45ba4fa02bbd19e80849c78c66fa7
Roman <67355674+shermanroman@users.noreply.github.com>
2020-07-10 21:04:42 +0800

    Update vk_bot_cod.py

--- vk_bot/vk_bot_cod.py
+++ vk_bot/vk_bot_cod.py
@@ -19,7 +19,11 @@ while True:
             print("Сообщение пришло от: " + str(event.user_id) + "\n")
             responde = event.text.lower()
             if event.from_user and not (event.from_me):
-                if responde != '1':
-                    vk_session.method('messages.send', {'user_id': event.user_id, 'message': 'Привет, друг! Попробуй написать 1', 'random_id': 0})
+                if responde == 'привет':
+                    vk_session.method('messages.send', {'user_id': event.user_id, 'message': 'О, супер! Так держать. Если напишешь "ты молодец", то заслужишь похвалу', 'random_id': 0})
+                elif responde == 'ты молодец':
+                    vk_session.method('messages.send', {'user_id': event.user_id, 'message': 'Супер! Ты заслужил похвалу! Если напишешь "пока", то я попращаюсь с тобой в ответ', 'random_id': 0})
+                elif responde == 'пока':
+                    vk_session.method('messages.send', {'user_id': event.user_id, 'message': 'Счастливо! Увидимся в следующих версиях!', 'random_id': 0})
                 else:
-                    vk_session.method('messages.send', {'user_id': event.user_id, 'message': 'Ты отлично справился, так держать!', 'random_id': 0})
+                    vk_session.method('messages.send', {'user_id': event.user_id, 'message': 'Привет, друг! Попробуй написать "привет"', 'random_id': 0})
